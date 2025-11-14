619ef3fe0d86aceaa1f7dd141a24e758571b55f0
enzomarioaiello <enzomarioaiello@gmail.com>
2025-11-14 20:38:53 +0100

    Added home image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,36 @@
 <script lang="ts">
     import { resolve } from '$app/paths';
+    import home from '$lib/images/home.jpg';
+
+    const homeImageAlt = "The Artist's Garden At Eragny by Camille Pissarro"
 </script>
 
 <div class="">
-    <h1>Enzo Aiello</h1>
-    <nav>
-        <a href={resolve("/")}>home</a>
-        <a href={resolve("/about")}>about</a>
-        <a href={resolve("/faq")}>faq</a>
-        <a href={resolve("/blog")}>blog</a>
-        <a href={resolve("/contact")}>contact</a>
-    </nav>
+    <header>
+        <h1>Enzo Aiello</h1>
+        <nav>
+            <a href={resolve("/")}>home</a>
+            <a href={resolve("/about")}>about</a>
+            <a href={resolve("/faq")}>faq</a>
+            <a href={resolve("/blog")}>blog</a>
+            <a href={resolve("/contact")}>contact</a>
+        </nav>
+    </header>
 
+    <main>
+        <section class="my-4 text-center">
+            <p>
+                <img
+                    src={home}
+                    alt="Beautiful"
+                    class="rounded-2xl"
+                />
+            </p>
+            <p class="py-2">
+                <em>
+                    {homeImageAlt}
+                </em>
+            </p>
+        </section>
+    </main>
 </div>
